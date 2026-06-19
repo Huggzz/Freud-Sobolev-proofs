5d8600e9aab0334dd0076daf09eded393adc89ba
HuggyHugMe <chugodu92@gmail.com>
2026-06-19 23:42:19 +0200

    update GP example

--- GP_eq/quadrature.jl
+++ GP_eq/quadrature.jl
@@ -1,9 +1,26 @@
-using IntervalArithmetic, Polynomials, PolynomialRoots, Serialization, LinearAlgebra
+using IntervalArithmetic, Polynomials, PolynomialRoots, Serialization, LinearAlgebra, Arblib, SpecialFunctions
 
 function enlarge(x::Interval{BigFloat})
     isguaranteed(x) || error("interval is not guaranteed")
     return interval(BigFloat(inf(x), RoundDown), BigFloat(sup(x), RoundUp))
 end
+
+
+SpecialFunctions.besseli(ν::Interval{BigFloat},z::Interval{BigFloat}) = interval(besseli(Arb(ν), Arb(z)))
+SpecialFunctions.besseli(ν::Rational,z::Interval{BigFloat}) = interval(besseli(interval(BigFloat, ν), z))
+
+function compute_M0(a, b)
+    # computes ∫exp(-ax^4/4+bx^2/2)dx
+    κ̂ = b^2/(interval(8)*a)
+    return interval(BigFloat, π)*sqrt(b/a)*exp(κ̂)*(besseli(-1//4, κ̂)+besseli(1//4, κ̂))/I"2"
+end
+
+function compute_M2(a, b)
+    # computes ∫x^2exp(-ax^4/4+bx^2/2)dx
+    κ̂ = b^2/(interval(8)*a)
+    return (b^2*(besseli(-1//4, κ̂)+besseli(3//4, κ̂)+besseli(5//4, κ̂))+(I"4"*a+b^2)*besseli(1//4, κ̂))*exp(κ̂)*interval(BigFloat, π)/(I"4"*sqrt(a^3*b))
+end
+
 
 function eval_poly(q, Y)
     n = length(Y)
@@ -20,15 +37,20 @@ end
 
 println("four-product quadrature rule:")
 
+m = 4
+κ = interval(BigFloat, 4)
 n = 2500
 N = 2*n+2
 setprecision(8192*16)
-b₁ = deserialize("b1_quad4")
+a = interval(BigFloat, m//2)
+b = κ*a
+b₁ = compute_M2(a, b)/compute_M0(a, b)
+Z = compute_M0(a, b)
 b₀ = interval(BigFloat, 0.0)
 b = zeros(Interval{BigFloat},N+1)
 b[1:2] = [b₀, b₁]
 for n = 2:N
-    b[n+1] = interval(BigFloat, 4)+(interval(BigFloat, n-1)/(interval(BigFloat, 2)*b[n]))-b[n]-b[n-1]
+    b[n+1] = κ+(interval(BigFloat, n-1)/(interval(BigFloat, m//2)*b[n]))-b[n]-b[n-1]
 end
 
 prec = 8192*2
@@ -36,8 +58,8 @@ setprecision(prec)
 a = enlarge.(sqrt.(b)[2:end])
 
 x = Polynomial(interval.(big.([0,1])))
-Pprev = Polynomial(interval.(big.([1]))./enlarge(sqrt(deserialize("GP_quad_Z4"))))
-P = Polynomial([interval(BigFloat, 0), interval(1)/a[1]]./enlarge(sqrt(deserialize("GP_quad_Z4"))))
+Pprev = Polynomial(interval.(big.([1]))./enlarge(sqrt(Z)))
+P = Polynomial([interval(BigFloat, 0), interval(1)/a[1]]./enlarge(sqrt(Z)))
 
 for i=2:N
     global P, Pprev = (x*P - a[i-1]*Pprev)/a[i], P
@@ -92,14 +114,14 @@ Xrig = interval.(inf.(Xl), sup.(Xu));
 
 # check a index below
 # quadrature wrt to the normalised measure
-W = (interval(2)/a[end])./(eval_poly(dp, Xrig).*eval_poly(pprev, Xrig).*Xrig)/deserialize("GP_Z")
+W = (interval(2)/a[end])./(eval_poly(dp, Xrig).*eval_poly(pprev, Xrig).*Xrig)/compute_M0(interval(BigFloat, 1), κ)
 
-b₁ = deserialize("b1_k4")
+b₁ = compute_M2(interval(BigFloat, 1), κ)/compute_M0(interval(BigFloat, 1), κ)
 b₀ = interval(BigFloat, 0.0)
 b = zeros(Interval{BigFloat},n+3)
 b[1:2] = [b₀, b₁]
 for k = 2:n+2
-    b[k+1] = interval(4)+(interval(BigFloat, k-1)/b[k])-b[k]-b[k-1]
+    b[k+1] = κ+(interval(BigFloat, k-1)/b[k])-b[k]-b[k-1]
 end
 
 setprecision(8192)
@@ -126,24 +148,29 @@ GC.gc()
 
 println("six-product quadrature rule:")
 
+m = 6
 n = 2500
 N = 3*n+2
 setprecision(8192*32)
-b₁ = deserialize("b1_quad6")
+a = interval(BigFloat, m//2)
+b = κ*a
+b₁ = compute_M2(a, b)/compute_M0(a, b)
+Z = compute_M0(a, b)
 b₀ = interval(BigFloat, 0.0)
 b = zeros(Interval{BigFloat},N+1)
 b[1:2] = [b₀, b₁]
 for n = 2:N
-    b[n+1] = interval(BigFloat, 4)+(interval(BigFloat, n-1)/(interval(BigFloat, 3)*b[n]))-b[n]-b[n-1]
+    b[n+1] = κ+(interval(BigFloat, n-1)/(interval(BigFloat, m//2)*b[n]))-b[n]-b[n-1]
 end
+
 
 prec = 8192*4
 setprecision(prec)
 a = enlarge.(sqrt.(b)[2:end])
 
 x = Polynomial(interval.(big.([0,1])))
-Pprev = Polynomial(interval.(big.([1]))./enlarge(sqrt(deserialize("GP_quad_Z6"))))
-P = Polynomial([interval(BigFloat, 0), interval(1)/a[1]]./enlarge(sqrt(deserialize("GP_quad_Z6"))))
+Pprev = Polynomial(interval.(big.([1]))./enlarge(sqrt(Z)))
+P = Polynomial([interval(BigFloat, 0), interval(1)/a[1]]./enlarge(sqrt(Z)))
 
 for i=2:N
     global P, Pprev = (x*P - a[i-1]*Pprev)/a[i], P
@@ -197,14 +224,14 @@ GC.gc()
 Xrig = interval.(inf.(Xl), sup.(Xu));
 
 # check a index below
-W = (interval(2)/a[end])./(eval_poly(dp, Xrig).*eval_poly(pprev, Xrig).*Xrig)/deserialize("GP_Z")
+W = (interval(2)/a[end])./(eval_poly(dp, Xrig).*eval_poly(pprev, Xrig).*Xrig)/compute_M0(interval(BigFloat, 1), κ)
 
-b₁ = deserialize("b1_k4")
+b₁ = compute_M2(interval(BigFloat, 1), κ)/compute_M0(interval(BigFloat, 1), κ)
 b₀ = interval(BigFloat, 0.0)
 b = zeros(Interval{BigFloat},n+3)
 b[1:2] = [b₀, b₁]
 for k = 2:n+2
-    b[k+1] = interval(4)+(interval(BigFloat, k-1)/b[k])-b[k]-b[k-1]
+    b[k+1] = κ +(interval(BigFloat, k-1)/b[k])-b[k]-b[k-1]
 end
 
 setprecision(8192*2)
